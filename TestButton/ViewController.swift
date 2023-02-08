e359b2abe7e7bde3ed71385c4a02f5c1cb4ec5c8
Jairo Munoz <jrbrzy@icloud.com>
2023-02-08 10:50:00 -0700

    Dope Button code

--- TestButton/ViewController.swift
+++ TestButton/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    @IBOutlet weak var Red: UIButton!
+    @IBOutlet weak var Blue: UIButton!
+    @IBOutlet weak var Green: UIButton!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        
+        
+        Red.addTarget(self, action: #selector(buttonPressed(button:)), for: .touchUpInside)
+        Green.addTarget(self, action: #selector(buttonPressed(button:)), for: .touchUpInside)
+        Blue.addTarget(self, action: #selector(buttonPressed(button:)), for: .touchUpInside)
+        
+        
     }
-
-
+    @objc func buttonPressed(button: UIButton) { print("button pressed")
+        
+        if button == Red {
+            view.backgroundColor = .red
+        }
+        if button == Green{
+                view.backgroundColor = .green
+            }
+            if button == Blue {
+                view.backgroundColor = .blue
+                
+                
+            }
+            
+            
+    }
 }
 
